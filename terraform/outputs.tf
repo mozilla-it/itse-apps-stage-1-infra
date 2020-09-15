2d0394f682b4683341f717dc232f3d9cf276918c
limed <limed@mozilla.com>
2020-09-15 15:47:00 -0700

    A little experiment to see if eip's can be attached to NLB

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,3 +8,7 @@ output "cluster_oidc_issuer_url" {
 output "cluster_name" {
   value = module.itse-apps-stage-1.cluster_id
 }
+
+output "refractr_eip_allocation_id" {
+  value = aws_eip.refractr_eip.id
+}
