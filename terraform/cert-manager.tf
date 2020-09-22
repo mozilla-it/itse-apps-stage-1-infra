417ef69901b46bcd83219cf71176e6759ce2dddb
limed <limed@mozilla.com>
2020-09-22 12:32:49 -0700

    Update ingress-controller and cert-manager

--- terraform/cert-manager.tf
+++ terraform/cert-manager.tf
@@ -1,5 +1,5 @@
 locals {
-  cert_manager_version      = "v1.0.1"
+  cert_manager_version      = "v1.0.2"
   cert_manager_crd_manifest = "https://github.com/jetstack/cert-manager/releases/download/${local.cert_manager_version}/cert-manager.crds.yaml"
 }
 
