caa287a0c359a4b9f7751a499be44d858ab611c8
Christina Harlow <cmharlow@gmail.com>
2021-06-09 16:19:05 -0400

    update tf modules pinned for updates in terraform-modules

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,24 +1,22 @@
 provider "aws" {
   region  = var.region
-  version = "~> 2"
+  version = "~> 3"
 }
 
 provider "kubernetes" {
-  version                = "~> 1"
+  version                = "~> 2"
   host                   = data.aws_eks_cluster.itse-apps-stage-1.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.itse-apps-stage-1.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.itse-apps-stage-1.token
-  load_config_file       = false
 }
 
 provider "helm" {
-  version = "~> 1"
+  version = "~> 2"
 
   kubernetes {
     host                   = data.aws_eks_cluster.itse-apps-stage-1.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.itse-apps-stage-1.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.itse-apps-stage-1.token
-    load_config_file       = false
   }
 }
 
