a91bd35e980a5b678bb6f5479f9abb8061755cec
Christina Harlow <cmharlow@gmail.com>
2021-06-19 19:45:12 -0400

    alphabetize lists, add papertrail logging, move nodes to private subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,52 +1,58 @@
 locals {
   cluster_features = {
-    "prometheus"         = true
+    "aws_calico"         = true
     "external_secrets"   = true
+    "fluentd_papertrail" = true
     "flux"               = true
     "flux_helm_operator" = true
-    "aws_calico"         = true
     "prometheus"         = true
-  }
-
-  flux_settings = {
-    "git.url"          = "git@github.com:mozilla-it/itse-apps-stage-1-infra"
-    "git.branch"       = "main"
-    "git.pollInterval" = "1m"
   }
 
   external_secrets_settings = {
     secrets_path = "/stage/*"
   }
 
-  subnet_az = [for s in data.aws_subnet.public : s.availability_zone]
-  subnet_id = [for s in data.aws_subnet.public : s.id]
+  fluentd_papertrail_settings = {
+    "externalSecrets.secretsKey" = "/stage/${module.itse-apps-stage-1.cluster_id}-papertrail"
+  }
+
+  flux_settings = {
+    "git.url"    = "git@github.com:mozilla-it/itse-apps-stage-1-infra"
+    "git.branch" = "main"
+  }
 
   node_groups = {
     green_node_group = {
       desired_capacity = 3,
-      min_capacity     = 3,
-      max_capacity     = 10,
-      instance_types   = ["t3.large"],
       disk_size        = 100,
-      subnets          = data.terraform_remote_state.vpc.outputs.public_subnets
+      instance_types   = ["t3.large"],
+      max_capacity     = 10,
+      min_capacity     = 3,
+      subnets          = data.terraform_remote_state.vpc.outputs.private_subnets
     }
   }
+
+  subnet_az = [for s in data.aws_subnet.public : s.availability_zone]
+  subnet_id = [for s in data.aws_subnet.public : s.id]
 }
 
 module "itse-apps-stage-1" {
-  source                    = "github.com/mozilla-it/terraform-modules//aws/eks?ref=master"
-  cluster_name              = "itse-apps-stage-1"
-  cluster_version           = "1.18"
-  vpc_id                    = data.terraform_remote_state.vpc.outputs.vpc_id
-  cluster_subnets           = data.terraform_remote_state.vpc.outputs.public_subnets
-  cluster_features          = local.cluster_features
-  node_groups               = local.node_groups
-  flux_settings             = local.flux_settings
-  external_secrets_settings = local.external_secrets_settings
-  enable_logging            = true
-  admin_users_arn           = ["arn:aws:iam::783633885093:role/maws-admin", "arn:aws:iam::517826968395:role/itsre-admin"]
+  source                      = "github.com/mozilla-it/terraform-modules//aws/eks?ref=master"
+  cluster_name                = "itse-apps-stage-1"
+  admin_users_arn             = ["arn:aws:iam::783633885093:role/maws-admin", "arn:aws:iam::517826968395:role/itsre-admin"]
+  cluster_features            = local.cluster_features
+  cluster_subnets             = data.terraform_remote_state.vpc.outputs.public_subnets
+  cluster_version             = "1.18"
+  enable_logging              = true
+  external_secrets_settings   = local.external_secrets_settings
+  fluentd_papertrail_settings = local.fluentd_papertrail_settings
+  flux_settings               = local.flux_settings
+  node_groups                 = local.node_groups
+  vpc_id                      = data.terraform_remote_state.vpc.outputs.vpc_id
 }
 
+# Chicken and egg issue, this needs to exist first
+# before we can create the refractr ingress-nginx
 resource "aws_eip" "refractr_eip" {
   count = length(local.subnet_az)
   vpc   = true
