e7e3be4168fc9b1daeefd22265f10967bed32ef3
Christina Harlow <cmharlow@gmail.com>
2021-06-22 09:10:32 -0400

    checking tf fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,18 +37,18 @@ locals {
 }
 
 module "itse-apps-stage-1" {
-  source                      = "github.com/mozilla-it/terraform-modules//aws/eks?ref=master"
-  cluster_name                = "itse-apps-stage-1"
-  admin_users_arn             = ["arn:aws:iam::783633885093:role/maws-admin", "arn:aws:iam::517826968395:role/itsre-admin"]
-  cluster_features            = local.cluster_features
-  cluster_subnets             = data.terraform_remote_state.vpc.outputs.public_subnets
-  cluster_version             = "1.18"
-  enable_logging              = true
-  external_secrets_settings   = local.external_secrets_settings
+  source                    = "github.com/mozilla-it/terraform-modules//aws/eks?ref=master"
+  cluster_name              = "itse-apps-stage-1"
+  admin_users_arn           = ["arn:aws:iam::783633885093:role/maws-admin", "arn:aws:iam::517826968395:role/itsre-admin"]
+  cluster_features          = local.cluster_features
+  cluster_subnets           = data.terraform_remote_state.vpc.outputs.public_subnets
+  cluster_version           = "1.18"
+  enable_logging            = true
+  external_secrets_settings = local.external_secrets_settings
   # fluentd_papertrail_settings = local.fluentd_papertrail_settings
-  flux_settings               = local.flux_settings
-  node_groups                 = local.node_groups
-  vpc_id                      = data.terraform_remote_state.vpc.outputs.vpc_id
+  flux_settings = local.flux_settings
+  node_groups   = local.node_groups
+  vpc_id        = data.terraform_remote_state.vpc.outputs.vpc_id
 }
 
 # Chicken and egg issue, this needs to exist first
