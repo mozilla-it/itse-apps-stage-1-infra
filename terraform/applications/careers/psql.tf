5734751e3f5ee323e8913ac114fa4ff1eaa8194f
Alan <duallain@mozilla.com>
2021-08-26 08:21:14 -0700

    Apply suggestions from code review
    Co-authored-by: Brett Kochendorfer <brett.kochendorfer@gmail.com>

--- terraform/applications/careers/psql.tf
+++ terraform/applications/careers/psql.tf
@@ -10,7 +10,7 @@ module "password" {
 }
 
 resource "aws_db_instance" "careers" {
-  identifier                  = "careers-${local.environment}"
+  identifier                  = "${local.project}-${local.environment}"
   storage_type                = "gp2"
   engine                      = "postgres"
   engine_version              = local.psql_version
@@ -19,7 +19,7 @@ resource "aws_db_instance" "careers" {
   max_allocated_storage       = local.psql_storage_max
   multi_az                    = local.psql_multiaz
   allow_major_version_upgrade = true
-  name                        = "careers"
+  name                        = local.project
   username                    = "careers"
   backup_retention_period     = 15
   db_subnet_group_name        = aws_db_subnet_group.careers-db.id
@@ -28,13 +28,13 @@ resource "aws_db_instance" "careers" {
 }
 
 resource "aws_db_subnet_group" "careers-db" {
-  name        = "careers-${local.environment}-db"
-  description = "Subnet Group for careers ${local.environment} DB"
+  name        = "${local.project}-${local.environment}-db"
+  description = "Subnet Group for ${local.project} ${local.environment} DB"
   subnet_ids  = flatten([data.terraform_remote_state.vpc.outputs.private_subnets])
 }
 
 resource "aws_security_group" "careers-db" {
-  name   = "careers-${local.environment}-db"
+  name   = "${local.project}-${local.environment}-db"
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
   ingress {
