64bc7e1107f0fd9fb1a522030d400fb072fdb228
Christina Harlow <charlow@mozilla.com>
2021-09-22 18:22:19 -0400

    remove hard coded old securitygroups on secwiki

--- terraform/applications/securitywiki/efs.tf
+++ terraform/applications/securitywiki/efs.tf
@@ -7,8 +7,6 @@ resource "aws_efs_mount_target" "securitywiki" {
   file_system_id = aws_efs_file_system.securitywiki.id
   subnet_id      = tolist(flatten([data.terraform_remote_state.vpc.outputs.private_subnets]))[count.index]
   security_groups = [
-    data.terraform_remote_state.k8s.outputs.cluster_primary_security_group_id,
-    "sg-083a35a6d382d52dc", # old cluster
-    "sg-0b9bcd2ae46fe0792"  # old cluster
+    data.terraform_remote_state.k8s.outputs.cluster_primary_security_group_id
   ]
 }
