fa519c81c7b32e713d3fc076333b5375a2121037
Christina Harlow <charlow@mozilla.com>
2021-10-04 11:05:43 -0400

    update discourse dev, stage to use latest helm chart with logger config as secret

--- terraform/applications/discourse/secrets.tf
+++ terraform/applications/discourse/secrets.tf
@@ -4,3 +4,9 @@ resource "aws_secretsmanager_secret" "envvar" {
   name        = "/${local.workspace.environment}/discourse/envvar"
   description = "discourse ${local.workspace.environment} environment variables as json (see helm chart for expected json)"
 }
+
+
+resource "aws_secretsmanager_secret" "logger" {
+  name        = "/${local.workspace.environment}/discourse/logger"
+  description = "discourse ${local.workspace.environment} papertrail logger configuration as json (see helm chart for expected json)"
+}
