5c5301b59a87b06c4e1b643acad54267278c735b
Rogerio Varalda <rvaralda@mozilla.com>
2021-12-16 19:41:32 +0100

    Security wiki decommissioning, remove terraform config

--- terraform/applications/securitywiki/secrets.tf
+++
@@ -1,6 +0,0 @@
-# Security Wiki Application Secrets Metadata
-
-resource "aws_secretsmanager_secret" "envvar" {
-  name        = "/${var.environment}/securitywiki/envvar"
-  description = "security wiki ${var.environment} environment variables as json (see helm chart for expected json)"
-}
